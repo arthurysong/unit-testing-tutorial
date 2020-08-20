a34bfdf0a1413fa85911ddca94d847249280b1aa
Arthur Song <arth3rs0ng@gmail.com>
2020-08-20 16:51:06 -0700

    Creating and styling index.js styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from './component/header';
+import './app.scss';
 
 function App() {
   return (
     <div className="App">
-      Test
+      <Header />
     </div>
   );
 }
---
+++ src/component/header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.scss'
+
+const Header = props => {
+    return <header>
+        <div className="wrap">
+            <div className="logo">
+                Logo
+            </div>
+        </div>
+    </header>
+};
+
+export default Header;
